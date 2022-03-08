e9c145750afd2e9e11a6b2eca342ee66203573f2
Jihan Park <parkjihan5253@gmail.com>
2022-03-08 16:11:34 +0900

    fix: notice query
    최근 게시물이 상단에 배치되도록 쿼리문 수정.

--- routes/main/Notice.js
+++ routes/main/Notice.js
@@ -6,7 +6,8 @@ exports.getNotices = async (req, res) => {
   const query = `SELECT n.title, n.detail, n.post_date, ud.year, ud.name
                   FROM  notice_board AS n
                   JOIN user_data AS ud
-                  ON n.post_writter = ud.id`
+                  ON n.post_writter = ud.id
+                  ORDER BY n.post_date desc`
   connection.query(query, (err, response) => {
     if (err) console.log(err);
 
