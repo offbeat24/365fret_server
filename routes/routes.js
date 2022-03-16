2c858b0f84bf2f947da98bc04a7942cf3bce31d8
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2022-03-17 03:09:02 +0900

    feat: getUserComingEvents, getUserPassedEvents

--- routes/routes.js
+++ routes/routes.js
@@ -11,7 +11,9 @@ router.put("/addEvent", event.addEvent);
 router.post("/deleteEvent", event.deleteEvent);
 
 // 렌더용 데이터 관리
-router.post("/getUserEvents", renderData.getUserEvents);
+//router.post("/getUserEvents", renderData.getUserEvents);
+router.post("/getUserComingEvents", renderData.getUserComingEvents);
+router.post("/getUserPassedEvents", renderData.getUserPassedEvents);
 router.post("/getUserProfile", renderData.getUserProfile);
 // 공지 관리
 router.get("/getNotices", notice.getNotices);
